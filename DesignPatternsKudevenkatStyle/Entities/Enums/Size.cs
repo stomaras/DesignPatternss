e1ee8bffb17cf9941ad62791fc57db274b152a85
stomaras <sptoma724@gmail.com>
2022-08-31 20:24:27 +0300

    Design Patterns

---
+++ DesignPatternsKudevenkatStyle/Entities/Enums/Size.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Enums
+{
+    public enum Size
+    {
+        XS,
+        S,
+        M,
+        L,
+        XL,
+        XXL,
+        XXXL
+    }
+}
---
+++ DesignPatternsKudevenkatStyle/Entities/TShirt.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class TShirt
+    {
+        public int Id { get; set; }
+        public Color Color { get; set; }
+        public Fabric Fabric { get; set; }
+        public Size Size { get; set; }
+    }
+}
